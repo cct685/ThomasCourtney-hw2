642b1d720533e225115c87ee531ed7a4cc3bce74
cct685 <ccthomas530@icloud.com>
2017-09-24 11:06:39 -0500

    final version

--- ThomasCourtney-hw2/ViewController.swift
+++ ThomasCourtney-hw2/ViewController.swift
@@ -8,29 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set each text field's delegate.
+        // This tells iOS that this class has a method iOS can call
+        // (textFieldShouldReturn) when it wants to dismiss the keyboard.
+        txtField?.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    // This method is called when the user touches the Return key on the
+    // keyboard. The 'textField' passed in is a pointer to the textField
+    // the cursor was in at the time they touched the Return key on the
+    // keyboard.
+    //
+    // From the Apple documentation: Asks the delegate if the text field
+    // should process the pressing of the return button.
+    //
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user touches on the main view (outside the UITextField).
+    //
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+   //label declarations
     @IBOutlet weak var label1: UILabel!
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var readyLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
-    
+    //text field assignments
     @IBOutlet weak var nameText: UITextField!
     
     @IBOutlet weak var cityText: UITextField!
-    
+    //button function
     @IBAction func myButton(_ sender: Any) {
         let name = nameText.text
         let city = cityText.text
@@ -45,7 +71,5 @@ class ViewController: UIViewController {
         }
     }
   
-    
-    
 }
 
